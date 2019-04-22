c8b95e3ceb517299ba873f1eac62ce01402d2af3
Biswajit Sahoo <biswajitsahoo.java@gmail.com>
2019-04-22 21:26:31 +0530

    added new changes

--- Sample_Project/src/rocky/scjp/durga/PlusOperator.java
+++ Sample_Project/src/rocky/scjp/durga/PlusOperator.java
@@ -5,21 +5,27 @@ public class PlusOperator {
 	public static void main(String[] args) {
 		
 		int i=2;
+		int j=3;
 		
 		i=i++;
+		j=j+1;
 		//It will increment but still it ll print the old value
 		//next time it ll print the increment value
-		System.out.println("1st --> "+i);
+		System.out.println("1st i -->"+i);
+		System.out.println("1st j -->"+j);
 		
 		i=i++;
-		
+		j=j+1;
 		//same as expected
-		System.out.println("2nd --> "+i);
+		System.out.println("2nd i -->"+i);
+		System.out.println("2nd j -->"+j);
 		
 		i=++i;
+		j=j-1;
 		// ++ operator will increse the value on same time ,
 		//it ll print the latest value as expected 
-		System.out.println("3rd --> "+i);
+		System.out.println("3rd i -->"+i);
+		System.out.println("3rd j -->"+j);
 
 	}
 
